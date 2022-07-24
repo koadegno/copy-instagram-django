accef142c1728bc0323581d71a7efaf53bfaf89f
koadegno <vinove9@hotmail.fr>
2022-07-24 17:13:13 +0200

    add core application

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model() 
+
+class Profile(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	id_user = models.IntegerField()
+	bio = models.TextField(blank=True)
+	phone = models.IntegerField(blank=True)
+	profile_image = models.ImageField(upload_to='profile_image', default='default_profile_picture.png')
+
+	def __str__(self) -> str:
+		profile = f"\n{self.user} ({self.user_id}) \n{self.phone}\n{self.bio}"
+		return super().__str__()+profile
